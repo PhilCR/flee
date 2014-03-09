cf38421ecdba2f2692a1d92196c14078ccf2ed4a
PhilCR <philippe.cesar.ramos@gmail.com>
2014-03-09 19:37:03 -0400

    v0.2

--- Assets/Scripts/BuildingEffect.js
+++ Assets/Scripts/BuildingEffect.js
@@ -1,17 +1,15 @@
 ﻿#pragma strict
 
 var audioSrc : AudioSource;
+var cube : GameObject;
 
 function Start () {
-	
 	renderer.material.color = new Color(Random.value, Random.value, Random.value);
-
 }
 
 function Update () {
-	
-	var spectrum : float[] = audioSrc.GetSpectrumData(64, 0, FFTWindow.BlackmanHarris);
+	var spectrum = audioSrc.GetSpectrumData(64,0, FFTWindow.BlackmanHarris);
 	var idx = Random.Range(0,64);
 	
-	
+	transform.localScale.y = Mathf.Clamp(spectrum[idx]*800,3,10);
 }
---
+++ Assets/Scripts/MusicalEffect.js
@@ -0,0 +1,62 @@
+﻿import System.IO;
+
+//Just to control the music
+var idx = 0;
+var mName : String;
+var mFolder = "/Music/";
+var filePaths : String[];
+
+var platform : GameObject;
+var goal : GameObject;
+var speed = 2;
+var audioSrc : AudioSource;
+
+function Start() {
+    
+    var localPath : String;
+    localPath = Application.dataPath;
+    localPath =	localPath.Replace("/Alpha.app/Contents","");
+    localPath =	localPath.Replace("/Assets","");
+    
+    if (Application.platform == RuntimePlatform.WindowsPlayer)
+
+		
+    if (Application.platform == RuntimePlatform.OSXEditor)
+    	localPath =	localPath.Replace("/Assets","");
+    
+    if (Application.platform == RuntimePlatform.WindowsEditor)
+    	localPath =	localPath.Replace("/Assets","");
+    
+    filePaths = Directory.GetFiles(localPath + mFolder,"*.ogg");
+    
+    if (filePaths == null){
+    	Debug.LogError("Null filePath on getting musics from fodler");
+    	return;
+    }
+    
+    PlayMusic(idx);
+     
+}
+
+function PlayMusic (idx){
+	var www = new WWW( "file://" + filePaths[idx]);
+    var clip : AudioClip = www.audioClip;
+    
+    mName = clip.name;
+    while(!clip.isReadyToPlay){
+    	yield 2;
+    }
+    
+    audioSrc.Stop();
+    audioSrc.clip = clip;
+    audioSrc.Play(); 
+    
+    var i;
+	var nPlatforms = audioSrc.clip.length / speed;
+	
+	for (i = 1; i < nPlatforms-1; i++){
+		Instantiate (platform, Vector3(-1.2, 0, i * 50), Quaternion.identity);
+	}
+	
+	//goal.transform.Translate(Vector3(0,-3,i*50));
+}
