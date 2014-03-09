4f5cb60262b6b1f04c73653832916c1fe49c34a0
PhilCR <philippe.cesar.ramos@gmail.com>
2014-03-08 20:40:51 -0500

    Again

--- Assets/Scripts/TransformFunctions.cs
+++ Assets/Scripts/TransformFunctions.cs
@@ -3,30 +3,26 @@ using System.Collections;
 
 public class TransformFunctions : MonoBehaviour
 {
-	public float moveSpeed = 0f;
-	public float turnSpeed = 50f;
+	public float moveSpeed = 15f;
 	public GameObject objectPlayer;
 	
 	void Update ()
 	{
 		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
-		moveSpeed += (0.2f* Time.deltaTime) ;
+//		moveSpeed += (0.2f* Time.deltaTime) ;
 
 		if (Input.GetKeyDown (KeyCode.C))
-			objectPlayer.transform.localScale = new Vector3 (0.5f,0.25f, 0.5f);
+			objectPlayer.transform.localScale = new Vector3 (1f,0.5f, 1f);
 
 		if (Input.GetKeyUp (KeyCode.C))
-			objectPlayer.transform.localScale = new Vector3 (0.5f,0.5f, 0.5f);
-			//transform.localScale = new Vector3 (1, Random.Range(1,6), 1);
-			//transform.localScale += new Vector3(0, 0.1F, 0);
-
+			objectPlayer.transform.localScale = new Vector3 (1f, 1f, 1f);
 
 		if(Input.GetKey(KeyCode.LeftArrow))
-			objectPlayer.transform.Translate(-Vector3.right * 2f * Time.deltaTime);
+			objectPlayer.transform.Translate(-Vector3.right * 3.5f * Time.deltaTime);
 		
 		if(Input.GetKey(KeyCode.RightArrow))
-			objectPlayer.transform.Translate(Vector3.right * 2f * Time.deltaTime);
+			objectPlayer.transform.Translate(Vector3.right * 3.5f * Time.deltaTime);
 
 		if(Input.GetKeyDown(KeyCode.Space))
 			objectPlayer.rigidbody.AddForce (Vector3.up * 250 );
